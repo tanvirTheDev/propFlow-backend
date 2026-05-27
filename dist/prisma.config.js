9d03037fb5d63874423a0b6b6d8a643221a55be3
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2026-05-27 17:17:18 +0600

    migrate issue

--- dist/prisma.config.js
+++ dist/prisma.config.js
@@ -9,7 +9,7 @@ exports.default = (0, config_1.defineConfig)({
         seed: 'npx tsx prisma/seed.ts',
     },
     datasource: {
-        url: process.env['DATABASE_URL'],
+        url: process.env['DIRECT_URL'] ?? process.env['DATABASE_URL'],
     },
 });
 //# sourceMappingURL=prisma.config.js.map
